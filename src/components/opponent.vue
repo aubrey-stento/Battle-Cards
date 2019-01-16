0fc036c6d450340d6a30ca864602dd9e3106ca57
Aubrey Stento <aubreystento@gmail.com>
2019-01-15 20:04:37 -0700

    fix aligning cards

--- src/components/opponent.vue
+++ src/components/opponent.vue
@@ -1,62 +1,63 @@
 <template>
-  <div v-if="game.id" class="opponent">
-      <div class="row">
-          <div class="col-6">
-          <h3>Opponents Hand</h3>
-          </div>
+    <div v-if="game.id" class="opponent">
+        <div class="row">
+            <div class="col-6">
+                <h3>Opponents Hand</h3>
+            </div>
         </div>
         <div class="row justify-content-center">
-              <div v-for="card in opponentsCards" @click="setActiveOpponent(card.id)">
-                  <div v-if="!card.visible">
-                      <img src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/1/1a/Card_back-Classic.png/200px-Card_back-Classic.png?version=029f4ee078258d159469720dc99127a2" height="250">
+            <div v-for="card in opponentsCards" @click="setActiveOpponent(card.id)">
+                <div v-if="!card.visible">
+                    <img src="https://d1u5p3l4wpay3k.cloudfront.net/hearthstone_gamepedia/thumb/1/1a/Card_back-Classic.png/200px-Card_back-Classic.png?version=029f4ee078258d159469720dc99127a2"
+                        height="250">
                     <!-- <h1>im a hidden card</h1> -->
-                  </div>
-                <div v-else class="card" height= "150" style="min-width: 12rem">
+                </div>
+                <div v-else class="card mb-2 mr-2" height="150" style="min-width: 12rem">
                     {{card.name}}
                     <img :src="card.img" height="150">
                     <p> Health: {{card.health}}</p>
-                    <p>Defense: {{card.defense}} </p>  
+                    <p>Defense: {{card.defense}} </p>
                     <p>Attacks: {{card.attack}}</p>
                 </div>
-                </div>
-              </div>
-          
+            </div>
         </div>
-      
 
-  </div>
+    </div>
+
+
+    </div>
 </template>
 
 <script>
-    
-  export default {
-    name: 'opponent',
-    data() {
-      return {
 
-      }
-    },
-    computed: {
-        game() {
-            return this.$store.state.game
+    export default {
+        name: 'opponent',
+        data() {
+            return {
+
+            }
         },
-        opponentsCards() {
-            return this.$store.state.game.opponent.hand
-        }
-    },
-    methods: {
-        // attack(attackName) {
-        //     let newAttack = {
-        //         attack: attackName
-        //     }
-        //     this.$store.dispatch("attack", {attack: newAttack, gameId: this.gameId})
-        // },
-        setActiveOpponent(opponentCard){
-            console.log(opponentCard)
-            this.$store.dispatch('setActiveOpponent', opponentCard)
+        computed: {
+            game() {
+                return this.$store.state.game
+            },
+            opponentsCards() {
+                return this.$store.state.game.opponent.hand
+            }
+        },
+        methods: {
+            // attack(attackName) {
+            //     let newAttack = {
+            //         attack: attackName
+            //     }
+            //     this.$store.dispatch("attack", {attack: newAttack, gameId: this.gameId})
+            // },
+            setActiveOpponent(opponentCard) {
+                console.log(opponentCard)
+                this.$store.dispatch('setActiveOpponent', opponentCard)
+            }
         }
     }
-  }
 
 </script>
 
--- src/components/player.vue
+++ src/components/player.vue
@@ -1,32 +1,34 @@
 <template>
-  <div v-if="game.id" class="player container-fluid">
-      <div class="row">
-              <h3>{{game.player.name}}'s Hand</h3>
-            </div>
-              <div class="row">
-              <div v-for="card in playersCards" @click="setActivePlayer(card.id)">
-                    <div class="card mb-2 mr-2" style="min-width: 12rem">
-                  {{card.name}}
-                  <img :src="card.img" height="150">
-
-                  <p> Health: {{card.health}}</p>
-                   <p>Defense: {{card.defense}} </p>  
-                   <p>Attacks: {{card.attack}}</p> 
-        
-                   
-                  
-
-                </div>
-              </div>
-            </div>
-          
+  <div v-if="game.id" class="player">
+    <div class="row">
+      <div class="col-6">
+        <h3>{{game.player.name}}'s Hand</h3>
       </div>
+    </div>
+    <div class="row justify-content-center">
+      <div v-for="card in playersCards" @click="setActivePlayer(card.id)">
+        <div class="card mb-2 mr-2" style="min-width: 12rem">
+          {{card.name}}
+          <img :src="card.img" height="150">
+
+          <p> Health: {{card.health}}</p>
+          <p>Defense: {{card.defense}} </p>
+          <p>Attacks: {{card.attack}}</p>
+
+
+
+
+        </div>
+      </div>
+    </div>
+
+  </div>
 
   </div>
 </template>
 
 <script>
-    
+
   export default {
     name: 'player',
     data() {
@@ -35,19 +37,19 @@
       }
     },
     computed: {
-        game() {
-            return this.$store.state.game
-        },
-        playersCards() {
-            return this.$store.state.game.player.hand
-        }
+      game() {
+        return this.$store.state.game
+      },
+      playersCards() {
+        return this.$store.state.game.player.hand
+      }
     },
     methods: {
-        
-        setActivePlayer(playerId){
-            console.log(playerId)
-            this.$store.dispatch('setActivePlayer', playerId)
-        }
+
+      setActivePlayer(playerId) {
+        console.log(playerId)
+        this.$store.dispatch('setActivePlayer', playerId)
+      }
     }
   }
 
